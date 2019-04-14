40fd98c7ffd670ee516d7de7365e422803e016ee
jpritin <jiprieto66@gmail.com>
2019-04-14 14:18:43 +0200

    Modificado borracontacto.php

--- borracontacto.php
+++ borracontacto.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form action="ejercicio07.php" method="post">
-Nombre: <input type="text" name="first"><br>
-Apellidos: <input type="text" name="last"><br>
-Email: <input type="text" name="email"><br>
-<input type="submit">
-</form>
+<?php
+// Datos para la conexión con MySql
+$servername = "localhost";
+$username = "usuario";
+$password = "12345";
+$dbname = "usuario";
+ 
+// Recogemos el "id" desde la URL
+$id = $_GET["id"];
+ 
+// Creamos la conexión con MySQL
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+ 
+// Comprobamos la conexión
+if (!$conn) {
+    die("Error de conexión con MySql: " .mysqli_connect_error());
+}
+ 
+$sql = "DELETE FROM contactos WHERE ID='$id'";
+ 
+if (mysqli_query($conn, $sql)) {
+    echo "Registro borrado";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+// Cerramos la conexión con MySQL
+mysqli_close($conn);
+?>
 </body>
 </html>
