3de4a6768ca784e3a03f97814dae17a72910b09b
jpritin <jiprieto66@gmail.com>
2019-04-14 14:01:47 +0200

    Cambiado ejercicio06formulario.php

--- ejercicio06formulario.php
+++ ejercicio06formulario.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form action="ejercicio05.php" method="post">
-Número: <input type="text" name="numero"><br>
+<form action="ejercicio06.php" method="post">
+Nombre: <input type="text" name="first"><br>
+Apellidos: <input type="text" name="last"><br>
+Email: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 </body>
