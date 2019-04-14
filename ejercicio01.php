552527ff19f58d6217fb8c2f637f0446bc4c138c
jpritin <jiprieto66@gmail.com>
2019-04-14 11:58:12 +0200

    subido ejercicio01

--- ejercicio01.php
+++ ejercicio01.php
@@ -2,6 +2,10 @@
 <html>
 <body>
 <?php
+/*
+Crea una página en php que muestra el mensaje “Hello World” 
+encerrado entre etiquetas “h1”
+*/
 $txt = "Hello";
 for($x = 1; $x <= 10; $x++){
     echo "<h1>".$txt."</h1>";
